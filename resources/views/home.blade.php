a58ae89e47e39e5ade04ec6278eee9504a3809e0
MembriniLuca29 <lucamembrini29@gmail.com>
2023-09-08 13:40:23 +0200

    inizializzazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.main')
 
-@section('page-title', 'Homepage')
+@section('page-title', 'Laravel Comics')
 
-{{-- @section('page-title')
-Homepage
-@endsection --}}
 
 @section('main-content')
 <h1>
     Homepage
 </h1>
 
-<div class="gatto"></div>
+{{-- <div class="gatto"></div>
 
-<img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt="">
+<img src="{{ Vite::asset('resources/img/gatto.jpg') }}" class="img-fluid" alt=""> --}}
 @endsection
