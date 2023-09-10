0f422f275172b0a81c81feb69b7b8f580db27df0
MembriniLuca29 <lucamembrini29@gmail.com>
2023-09-10 04:12:22 +0200

    esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,16 @@
 
 <div class="card">
     <div class="row g-0">
-      <div class="col-md-4">
+      <div class="col-md-4 d-flex align-items-center">
         <img src="{{ $comic['thumb'] }}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">{{ $comic['title'] }}</h5>
           <p class="card-text">click <a href="">here</a> for description</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          <p class="card-text">Price: {{ $comic['price'] }}</p>
+          <p class="card-text"><small class="text-body-secondary">Series: {{ $comic['series'] }}</small></p>
+          <p class="card-text"><small class="text-body-secondary">Sale date  {{ $comic['sale_date'] }}</small></p>
         </div>
       </div>
     </div>
@@ -26,5 +28,4 @@
 
     @endforeach
 </div>
-<a href="{{ route('comics') }}">comics</a>
 @endsection
